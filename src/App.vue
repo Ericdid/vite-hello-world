33182fcfe86af6d8d586385def49d97e8dd88e4c
Ericdid <ericdidom@gmail.com>
2023-09-07 22:16:53 +0200

    add.start-adding.image

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <script>
+import AppHero from "./components/AppHero.vue";
 export default {
   data() {
     return {
       AppTitle: "La mia prima app con Vite!",
     };
   },
+  components: { AppHero },
 };
 </script>
 
 <template>
-  <h1 class="center-align">{{ AppTitle }}</h1>
-  <hr />
+  <AppHero />
+  <div class="wrapper">
+    <h1 class="center-align">{{ AppTitle }}</h1>
+    <hr />
+  </div>
 </template>
 
-<style></style>
+<style lang="scss">
+.wrapper {
+  background-color: rgb(64, 64, 78);
+  h1 {
+    color: rgb(245, 245, 245);
+  }
+}
+</style>
